6ee4efa29b4ca9155d57de8d622cc359d5bb8c1b
Sashy21 <71380766+Sashy21@users.noreply.github.com>
2020-09-20 22:13:19 +0300

    Add files via upload
    The following codes illustrate the overflows and underflows of various datatypes in C++
    The second code shows different displays depending on the specification in brackets

---
+++ DSA Int.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	a = 5000000000;
+	cout<<a<<endl;
+	
+	int b;
+	b = -33000;
+	cout<<b<<endl;
+	
+	return 0;
+}
---
+++ DSA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	char x;
+	x=300;
+	cout<<x<<endl;
+	 char w;
+	 w=-130;
+	 cout<<w<<endl;
+	return 0;
+
+}
+
